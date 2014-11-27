d5628f1fe545f1cf26f364544cb139aafaba4b9c
Takuya ASADA <syuu@cloudius-systems.com>
2014-11-28 05:31:46 +0900

    Add reactor::connect(), client_socket definition and network stack stub code
    Signed-off-by: Takuya ASADA <syuu@cloudius-systems.com>

--- core/reactor.hh
+++ core/reactor.hh
@@ -213,7 +213,6 @@ public:
     virtual future<connected_socket, socket_address> accept() = 0;
 };
 
-
 namespace std {
 
 template <>
@@ -241,6 +240,7 @@ class network_stack {
 public:
     virtual ~network_stack() {}
     virtual server_socket listen(socket_address sa, listen_options opts) = 0;
+    virtual connected_socket connect(socket_address sa) = 0;
     virtual net::udp_channel make_udp_channel(ipv4_addr addr = {}) = 0;
     virtual future<> initialize() {
         return make_ready_future();
@@ -661,9 +661,11 @@ public:
 
     server_socket listen(socket_address sa, listen_options opts = {});
 
+    connected_socket connect(socket_address sa);
+
     pollable_fd posix_listen(socket_address sa, listen_options opts = {});
 
-    future<pollable_fd> posix_connect(socket_address sa);
+    pollable_fd posix_connect(socket_address sa);
 
     future<pollable_fd, socket_address> accept(pollable_fd_state& listen_fd);
 
